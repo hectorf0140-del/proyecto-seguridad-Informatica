e3a142d08c7c566e8836009d9097922de57d9e2b
hectorf0140-del <hectorf0.14.0@gmail.com>
2026-07-25 20:19:07 -0600

    Delete proyecto seguridad informatica directory

--- proyecto seguridad informatica/config.js
+++
@@ -1,5 +0,0 @@
-// ⚠️ Completá estos dos valores antes de publicar el sitio.
-const CONFIG = {
-  TMDB_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMWEwOWZmNzkyZjMyZmIzYWQ4ZDc4ZmRiOGU4ZGVmNSIsIm5iZiI6MTc4NTAyOTQ1MC43NzcsInN1YiI6IjZhNjU2MzRhNjZiZjA3M2VhMjg2ZTlmYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iUeya6dEKa81LFeFybPVTOIYn3UbZimzGpOHR4056pE",
-  GOOGLE_CLIENT_ID: "TU_GOOGLE_CLIENT_ID_AQUI.apps.googleusercontent.com"
-};
